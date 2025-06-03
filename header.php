94f3fb29fb6dc03b4f102d5f616930e543f24bcc
alberto231090 <161619919+alberto231090@users.noreply.github.com>
2025-06-03 10:20:22 +0200

    Update header.php

--- header.php
+++ header.php
@@ -3,7 +3,7 @@
 <head>
   <meta charset="<?php bloginfo( 'charset' ); ?>">
   <meta name="viewport" content="width=device-width, initial-scale=1">
-<title>Ultime news sul mondo della WEC</title>
+
   <meta name="keywords" content="Formula 1, Ferrari, F1, motorsport, Leclerc, Hamilton">
   
   <?php wp_head(); ?>
